39414ad1a4610a38b96b8d5d449a9e13a6df5c9f
Samuel Jeon <soulstar67@gmail.com>
2022-12-22 12:19:54 -0600

    Component, props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+function Header(props) {
+  return       <header>
+  <h1><a href="/" onClick={(event)=>{
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
+</header>
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length;i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+      props.onChangeMode(t.id); //props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)
+
+  }
+  return <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>
+}
+
+function Article(props) {
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
 
 function App() {
+  const mode = 'WELCOME';
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
+  let content = null;
+  if (mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ') {
+    content = <Article title="Welcome" body="Hello, READ"></Article>
+  }
   return (
     <div>
-      <header>
-        <h1><a href="">WEB</a></h1>
-      </header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/1">css</a></li>
-          <li><a href="/read/1">js</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
